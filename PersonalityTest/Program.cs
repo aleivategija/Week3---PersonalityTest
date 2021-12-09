afb341623c0b2ee1ffeb14fa5275e3e8142c1a64
Artur <aleivate@tktk.ee>
2021-12-09 20:02:26 +0200

    Personality Test

---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma lemmikv2rv:");
+
+            string userColor = Console.ReadLine();
+
+            if (userColor == "punane")
+            {
+                Console.WriteLine("Oled romantiline.");
+            }
+            else if (userColor == "sinine")
+            {
+                Console.WriteLine("Oled t66kas.");
+            }
+            else if (userColor == "roheline")
+            {
+                Console.WriteLine("Oled looduse s6ber");
+            }
+            else
+            {
+                Console.WriteLine($"Oled {userColor} ykssarvik");
+            }
+
+            Console.WriteLine("Kena p2eva!");
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma lemmikv2rv:");
+
+            string userColor = Console.ReadLine().ToLower();
+
+            switch(userColor)
+            {
+                case "punane":
+                    Console.WriteLine("Oled romantiline");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled t66kas");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled looduse s6ber");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ykssarvik");
+                    break;
+            }
+
+            Console.WriteLine("Kena p2eva!");
+        }
+    }
+}
